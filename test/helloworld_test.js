1d8ef85a059150af0f24ddbcb34e1accadaec830
Emanuele Gentiletti <emanuele.gentiletti@gmail.com>
2018-04-08 19:58:47 +0200

    Helloworld application

---
+++ test/helloworld_test.js
@@ -0,0 +1,22 @@
+const Browser = require('zombie')
+const assert = require('assert')
+
+const app = require('../helloworld')
+
+const site = 'http://localhost:3000'
+
+describe('main page', function()  {
+
+  before(function() {
+    this.browser = new Browser({ site })
+  })
+
+  before(function(done) {
+    this.browser.visit('/', done)
+  })
+
+  it('should say hello world', function () {
+    assert.ok(this.browser.success)
+    assert.equal(this.browser.text(), "Hello world")
+  })
+})
